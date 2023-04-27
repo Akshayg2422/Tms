190496c5abb6552b2a498d145caf32373f459d2e
Jay <jayakumar@leorainfotech.in>
2023-04-27 17:25:54 +0530

    Task close

--- src/Components/Component/Status/index.tsx
+++ src/Components/Component/Status/index.tsx
@@ -4,10 +4,10 @@ type statusProps = {
     status: any
   }
 function Status({ status }:statusProps) {
-    const color = getObjectFromArrayByKey(STATUS_LIST, 'id', status).color
+    const color = getObjectFromArrayByKey(STATUS_LIST, 'id', status)?.color
     return <div className="row mb-0 align-items-center">
        <div style={{ height: 10, width: 10, borderRadius: 5, background: color, color:color}}> </div>
-      <span className="ml-2">{getObjectFromArrayByKey(STATUS_LIST, 'id', status).text} </span>
+      <span className="ml-2">{getObjectFromArrayByKey(STATUS_LIST, 'id', status)?.text} </span>
     </div>
   }
   export {Status}
