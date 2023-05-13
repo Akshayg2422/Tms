5e7e3ceabb30a6eafdba2350424bb70894957466
Jay <jayakumar@leorainfotech.in>
2023-05-13 18:55:42 +0530

    no message

--- src/Components/Core/Back/interfaces.ts
+++ src/Components/Core/Back/interfaces.ts
@@ -1,3 +1,5 @@
 export interface BackProps {
     icon?: any,
+    
+    
 }
--- src/Hooks/useNavigation/index.ts
+++ src/Hooks/useNavigation/index.ts
@@ -8,7 +8,7 @@ const useNavigation = () => {
     navigation(to, { replace: replace });
 
 
-  const goBack= () => navigation(-1)
+  const goBack= (action:any= -1) => navigation(action)
   return { goTo , goBack, params};
 
 };
