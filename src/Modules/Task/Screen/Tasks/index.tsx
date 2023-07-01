8d3085046e6ace989836d4d3e0e0056698ebfe33
Jay <jayakumar@leorainfotech.in>
2023-07-01 15:49:02 +0530

    no message

--- src/Modules/Task/Screen/Tasks/index.tsx
+++ src/Modules/Task/Screen/Tasks/index.tsx
@@ -42,12 +42,11 @@ function Tasks() {
 
   const normalizedTableData = (data: any) => {
     if (data && data?.length > 0)
-      return data?.map((el: any, index: number) => {
+      return data?.map((el: any,index:number) => {
 
         const { priority, parent, task_attachments, by_user, raised_by_company, created_at, task_status, eta_time, title, assigned_to, description } = el
 
         return {
-          key: el.id,
           "task":
             <>
               <div className="row">
@@ -147,7 +146,6 @@ function Tasks() {
 
           {tasks && tasks.length > 0 ?
             <CommonTable
-              key={taskCurrentPages}
               isPagination
               tableDataSet={tasks}
               displayDataSet={normalizedTableData(tasks)}
