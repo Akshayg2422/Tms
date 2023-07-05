2a3abaabc7c85bac2e3843017aa18c5adf111c4b
LEORA\sanka <sankarveera.intern@leorainfotech.in>
2023-07-05 12:19:47 +0530

    no message

--- src/Modules/UserCompany/Container/CarouselImages/index.tsx
+++ src/Modules/UserCompany/Container/CarouselImages/index.tsx
@@ -1,5 +1,5 @@
 import { Image } from "@Components";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { CarouselImagesProps } from './interfaces';
 import { getPhoto, getDisplayTimeDateMonthYearTime, getMomentObjFromServer } from "@Utils";
 import { Carousel } from "react-responsive-carousel";
@@ -11,39 +11,42 @@ function CarouselImages({ item }: CarouselImagesProps) {
         item;
 
 
+
     return (
         <>
+            <div className="row mx--5">
+                <div className="col">
+                    <div className="row" >
+                        <div className="col-lg-12 col-sm-0 col-12 mt-3 mb--6">
+                            <Carousel
+                                dynamicHeight
+                            >
+                                {attachments &&
+                                    attachments.length > 0 &&
+                                    attachments?.map((attachment_logo: any, index: number) => {
+                                        return (
+                                            <Image
+                                                variant={"default"}
+                                                src={getPhoto(attachment_logo.attachment_file)}
+                                                height={'80%'}
+                                                width={'80%'}
+                                            />
+                                        );
+                                    })}
+                            </Carousel>
+                        </div>
+                    </div>
+                    <div className="row ml-2 mt-4">
+                        <div className="text-xs"> {place} </div>
+                    </div>
+                    <div className="row ml-2 text-xs">
+                        <span> {getDisplayTimeDateMonthYearTime(getMomentObjFromServer(start_time))}  {" "} {'-'} {" "} </span>
+                        <span>  {" "} {' '} {" "} {getDisplayTimeDateMonthYearTime(getMomentObjFromServer(end_time))} </span>
+                    </div>
+                </div>
 
-            <div className="" >
-                <div className="">
-                    <Carousel
-                        dynamicHeight 
-                    >
-                        {attachments &&
-                            attachments.length > 0 &&
-                            attachments?.map((attachment_logo: any, index: number) => {
-                                return (
-                                    <Image
-                                        variant={"default"}
-                                        src={getPhoto(attachment_logo.attachment_file)}
-                                        height={'80%'}
-                                        width={'80%'}
-                                    />
-                                );
-                            })}
-                    </Carousel>
-                </div>
             </div>
-            <div className="">
-                <div className="row ml--4 ">
-                    <div className="text-xs"> {place} </div>
-                </div>
 
-                <div className="row ml--4 text-xs">
-                    <span> {getDisplayTimeDateMonthYearTime(getMomentObjFromServer(start_time))}  {" "} {'-'} {" "} </span>
-                    <span>  {" "} {' '} {" "} {getDisplayTimeDateMonthYearTime(getMomentObjFromServer(end_time))} </span>
-                </div>
-            </div>
 
         </>
 
