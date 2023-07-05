ce8d667f6b31775413fa34b54f3ac396be038f81
Jayakumar <jayakumar@leorainfotech.in>
2023-07-05 12:18:24 +0530

    code

--- src/Components/Core/Button/index.tsx
+++ src/Components/Core/Button/index.tsx
@@ -1,35 +1,52 @@
 import React from 'react';
 import { Button as RSButton } from 'reactstrap';
 import { ButtonProps } from './interfaces';
-import { Image } from '@Components'
+import { Image, Spinner } from '@Components'
 
 function Button({
-  text, color = 'primary', variant = 'default', size = 'md', height="15",width="15", icon, onEnter, ...rest
+  loading, text, color = 'primary', variant = 'default', size = 'md', height = 15, width = 15, icon, onEnter, onClick, ...rest
 }: ButtonProps) {
 
   return (
     <>
-      {variant === 'default' && <RSButton type={'button'} size={size} color={color} {...rest} >{text}</RSButton>}
+      {variant === 'default' && <RSButton type={'button'} size={size} color={color} {...rest} onClick={loading ? undefined : (e) => {
+
+        if (onClick) {
+          onClick(e)
+        }
+
+      }} >
+        {
+
+          loading && <Spinner />
+        }
+        {!loading && text}
+
+      </RSButton>}
 
       {(variant === 'icon' || variant === 'icon-with-text') &&
         <RSButton type={'button'} size={size} className={'btn-icon'} color={color} {...rest} >
           {
-            <span className={`btn-inner--icon ${variant === 'icon-with-text' && 'mr-1'}`}>
-              <i className='ni ni-atom' />
-            </span>
+
+            loading && <Spinner />
           }
-          {
-            variant === 'icon-with-text' &&
-            <span className={'btn-inner--text'}>{text}</span>
+          {!loading && <>
+            {
+              <span className={`btn-inner--icon ${variant === 'icon-with-text' && 'mr-1'}`}>
+                <i className='ni ni-atom' />
+              </span>
+            }
+            {
+              variant === 'icon-with-text' &&
+              <span className={'btn-inner--text'}>{text}</span>
+            }
+          </>
           }
         </RSButton>
       }
 
       {variant === 'icon-rounded' &&
         <RSButton type={'button'} size={size} className={'btn-icon-only rounded-circle d-flex align-items-center justify-content-center'} color={color}  {...rest} >
-          {/* <span className={'btn-inner--icon'}>
-            <i className='ni ni-atom' />
-          </span> */}
           <Image src={icon} height={height} width={width} />
         </RSButton>
       }
--- src/Components/Core/Spinner/index.tsx
+++ src/Components/Core/Spinner/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { SpinnerProps } from './interfaces'
 import { Spinner as RsSpinner } from 'reactstrap'
 
-function Spinner({ color = 'black' }: SpinnerProps) {
+function Spinner({ color = 'white' }: SpinnerProps) {
     return (
         <RsSpinner color={color} size={'sm'} >
             Loading...
