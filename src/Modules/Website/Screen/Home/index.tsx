11722cb7f3cdb97844ed8bb4101603ed299fd81c
LEORA\sanka <sankarveera.intern@leorainfotech.in>
2023-08-08 15:22:35 +0530

    no message

--- src/Modules/Website/Screen/Home/index.tsx
+++ src/Modules/Website/Screen/Home/index.tsx
@@ -8,27 +8,27 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
  
-      <div className="pt-6 pb-9 bg-customGradient-primary " >
+      <div className="pt-7 pb-9 bg-customGradient-primary" >
         <Container>
-          <div className="header-body scrolled fade-in-bottom">
+          <div>
             <Row className="align-items-center">
-              <Col lg="6" className='justify-content-center text-center d-flex pt-6'>
-                <div className="">
-                  <h2 className=" display-4 text-white font-weight-bold  ">
+              <Col lg="6" className='justify-content-center d-flex mt-5 scrolled fade-in-bottom'>
+                <div className="scrolled fade-in-bottom fade-in">
+                  <h1 className="display-4 mx-2 text-white font-weight-bold ">
                     Complete Task & Ticket
                     management Web & Mobile App
-                  </h2>
-                  <p className="text-white mt-4 text-justify">
+                  </h1>
+                  <p className="text-white mt-4 text-justify ">
                     Quanta TMS transforms task and ticket management, boosting productivity and collaboration.
                     Create swift resolutions, embrace dynamic news feeds, and enhance remote teamwork.
                     With comprehensive timesheets and efficient communication, Quanta TMS shapes the future of collaboration.
                   </p>
                 </div>
               </Col>
-              <Col lg="6" className='pt-4 justify-content-center d-flex'>
+              <Col lg="6" className='pt-4 justify-content-center d-flex scrolled fade-in-bottom mt--5'>
                 <Image
                   src={icons.TMS_home}
-                  // height={"100%"}
+                  height={"100%"}
                   width={"110%"}
                   className=''
 
