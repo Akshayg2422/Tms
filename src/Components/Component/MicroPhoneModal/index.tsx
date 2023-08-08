c2e344137ac5a4ae3b376dc63fef36e249232194
INBOOK_X1_SLIM\navee <naveen.intern@leorainfotech.in>
2023-08-08 15:47:08 +0530

    no message

--- src/Components/Component/MicroPhoneModal/index.tsx
+++ src/Components/Component/MicroPhoneModal/index.tsx
@@ -20,7 +20,6 @@ function MicroPhoneModal({selectedModal=false}:MicroPhoneProps) {
     const [audioData,setAudioData]=useState()
     const { selectedMicroModal} = useSelector((state: any) => state.TaskReducer);
     const { dashboardDetails } = useSelector((state: any) => state.UserCompanyReducer);
-    console.log(JSON.stringify(dashboardDetails),"dashboardDetails ==>")
     const microPhoneModals=useModal(true)
     const loginLoader = useLoader(false);
     const [counting,setCounting]=useState(false)
@@ -238,7 +237,7 @@ function MicroPhoneModal({selectedModal=false}:MicroPhoneProps) {
       }
 
 {recording && <Button text={'Stop'}onClick={()=>{
-        stopVoiceRecording()
+         stopVoiceRecording()
         setStopAudio(false)
         }} size={'sm'}/>}
 
